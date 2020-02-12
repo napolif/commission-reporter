2e1205c130923dee35b3cdce5e6f5e34c14c646a
John Cipriano <johnmikecip@gmail.com>
2020-02-12 02:42:31 -0500

    add & generate devise & active_admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # for devise
+  # TODO: set up other environments if needed
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
