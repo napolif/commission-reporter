2f38be0eee5ace26d9d11841ca227b0a86981831
John Cipriano <johnmikecip@gmail.com>
2020-02-13 12:00:53 -0500

    annotate models

--- app/models/sales_rep.rb
+++ app/models/sales_rep.rb
@@ -1,3 +1,40 @@
+# == Schema Information
+#
+# Table name: sales_reps
+#
+#  id         :bigint           not null, primary key
+#  code       :string
+#  name       :string
+#  comm_type  :string
+#  period1    :decimal(8, 2)
+#  period2    :decimal(8, 2)
+#  period3    :decimal(8, 2)
+#  period4    :decimal(8, 2)
+#  period5    :decimal(8, 2)
+#  goal1      :decimal(8, 2)
+#  goal2      :decimal(8, 2)
+#  goal3      :decimal(8, 2)
+#  goal4      :decimal(8, 2)
+#  goal5      :decimal(8, 2)
+#  goal6      :decimal(8, 2)
+#  goal7      :decimal(8, 2)
+#  goal8      :decimal(8, 2)
+#  goal9      :decimal(8, 2)
+#  goal10     :decimal(8, 2)
+#  comm1      :decimal(8, 2)
+#  comm2      :decimal(8, 2)
+#  comm3      :decimal(8, 2)
+#  comm4      :decimal(8, 2)
+#  comm5      :decimal(8, 2)
+#  comm6      :decimal(8, 2)
+#  comm7      :decimal(8, 2)
+#  comm8      :decimal(8, 2)
+#  comm9      :decimal(8, 2)
+#  comm10     :decimal(8, 2)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class SalesRep < ApplicationRecord
   validates :code, presence: true, uniqueness: {case_sensitive: false}
   validates :name, presence: true
