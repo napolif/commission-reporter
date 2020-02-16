1a32191e3ee02af5e4f14d0be3ebcdf701feb95b
John Cipriano <johnmikecip@gmail.com>
2020-02-15 21:31:56 -0500

    remove commented out dashboard stuff

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,25 +4,10 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        # span I18n.t("active_admin.dashboard_welcome.welcome")
-        # small I18n.t("active_admin.dashboard_welcome.call_to_action")
         img src: '/napoli.png', height: '80px'
         img src: '/ferraro.svg', height: '80px'
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
 
     columns do
       column do
