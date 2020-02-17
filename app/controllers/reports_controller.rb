ddd39fd060a7756694d94beec6e5258a477ab443
John Cipriano <johnmikecip@gmail.com>
2020-02-16 23:21:39 -0500

    handle missing sales reps
    add & use ReportShowPresenter; tweak report styling

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,27 +2,12 @@ class ReportsController < ApplicationController
   before_action :cast_boolean_params
 
   def show
-    @data = report_data
     @one_per_page = true if params[:one_per_page]
     @list_disabled_reps = true if params[:list_disabled_reps]
+    @presenter = ReportsShowPresenter.new(params[:batch])
   end
 
   private
-
-  def report_data
-    invs_by_rep = invoices.group_by(&:sales_rep)
-    invs_by_rep.transform_values do |invs|
-      invs.map { |inv| Commission.new(inv) }
-    end
-  end
-
-  def invoices
-    if params[:batch] == "latest"
-      Invoice.latest.includes(:sales_rep)
-    else
-      Invoice.where(batch: params[:batch]).includes(:sales_rep)
-    end
-  end
 
   def cast_boolean_params
     cast_boolean_param(:grayscale)
---
+++ app/presenters/reports_show_presenter.rb
@@ -0,0 +1,90 @@
+# Collected data for rendering ReportsController#show.
+class ReportsShowPresenter
+  extend Memoist
+
+  attr_reader :batch_num
+
+  def initialize(batch_num)
+    @batch_num = batch_num
+  end
+
+  def commissions_by_enabled_rep
+    enabled_rep_codes.each_with_object({}) do |code, hash|
+      key = reps_by_code[code]
+      value = commissions_by_code[code]
+      hash[key] = value
+    end
+  end
+
+  def total_rows
+    enabled_rep_codes.map do |code|
+      [reps_by_code[code], totals_by_code[code]]
+    end.sort_by { |row| row.first.code }.reverse.sort_by(&:second).reverse
+  end
+
+  def grand_total
+    totals_by_code.values.reduce(:+)
+  end
+
+  def disabled_reps
+    disabled_rep_codes.map do |code|
+      reps_by_code[code]
+    end
+  end
+
+  private
+
+  def enabled_rep_codes
+   rep_codes.reject do |code|
+      reps_by_code[code]&.disabled
+    end
+  end
+
+  def disabled_rep_codes
+    rep_codes.filter do |code|
+      reps_by_code[code]&.disabled
+    end
+  end
+
+  def commissions_by_code
+    invoices_by_code.transform_values do |invs|
+      invs.map { |inv| Commission.new(inv) }
+    end
+  end
+  memoize :commissions_by_code
+
+  def totals_by_code
+    commissions_by_code.transform_values do |comms|
+      comms.map(&:amount).reduce(:+)
+    end
+  end
+  memoize :totals_by_code
+
+  def reps_by_code
+    known_reps_by_code = SalesRep.all.index_by(&:code)
+
+    rep_codes.each_with_object({}) do |code, hash|
+      hash[code] = known_reps_by_code[code] || SalesRep.default_new(code)
+    end
+  end
+  memoize :reps_by_code
+
+  def rep_codes
+    invoices_by_code.keys.sort
+  end
+  memoize :rep_codes
+
+  def invoices_by_code
+    invoices.group_by(&:sales_rep_code)
+  end
+  memoize :invoices_by_code
+
+  def invoices
+    if batch_num == "latest"
+      Invoice.latest_batch
+    else
+      Invoice.where(batch: batch_num)
+    end.includes(:sales_rep)
+  end
+  memoize :invoices
+end
