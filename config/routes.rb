64626ace61c3318fd6e0559ce6659a825cce660e
John Cipriano <johnmikecip@gmail.com>
2020-02-28 14:33:23 -0500

    change by date route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :reports, only: [:index] do
     collection do
       get 'batch/:batch_id', action: :batch, as: 'by_batch'
-      # get 'date/:date_from/to/:date_to', action: :date, as: 'by_date'
+      get 'date', action: :date, as: 'by_date'
     end
   end
 
