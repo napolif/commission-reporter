cfba95cd895f78c7785a64fa68fed11d4831c864
John Cipriano <johnmikecip@gmail.com>
2020-02-27 22:12:20 -0500

    routes: home to reports index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root to: "admin/dashboard#index"
+  root to: "reports#index"
 
   resources :reports, only: [:index] do
     collection do
