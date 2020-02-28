c90561c769548a3de0be0dba623f314daeec24c6
John Cipriano <johnmikecip@gmail.com>
2020-02-27 22:11:15 -0500

    put ferraro logo first

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,8 +4,8 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        img src: "/napoli.png", height: "80px"
         img src: "/ferraro.svg", height: "80px"
+        img src: "/napoli.png", height: "80px"
       end
     end
 
