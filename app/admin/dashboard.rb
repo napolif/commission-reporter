0148141139bbc9f2bf71516e70503bc9292980b4
John Cipriano <johnmikecip@gmail.com>
2020-02-28 22:18:10 -0500

    remove dashboard report links & options (& javascript)

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -15,32 +15,5 @@ ActiveAdmin.register_page "Dashboard" do
       latest_date = Invoice.latest_batch.pluck(:invoiced_on).max.to_s
       para "Latest invoice from: " + latest_date, class: "statistic"
     end
-
-    panel "Reports" do
-      ul class: "reportList" do
-        Invoice.batch_numbers.sort.reverse.each do |num|
-          li class: "reportListItem" do
-            span "Commission Report - #{num}", class: "reportName"
-            span link_to("HTML", by_batch_reports_path(num),
-                         class: "formatLink-html js-htmlLink")
-            span link_to("PDF", by_batch_reports_path(num, format: :pdf),
-                         class: "formatLink-pdf js-pdfLink")
-            span link_to("CSV", by_batch_reports_path(num, format: :csv),
-                         class: "formatLink-csv")
-          end
-        end
-      end
-    end
-
-    panel "Options", class: "optionsPanel" do
-      input type: "checkbox", class: "js-listDisabledReps", name: "list_disabled_reps"
-      label "List disabled reps in summary", for: "list_disabled_reps"
-      br
-      input type: "checkbox", class: "js-onePerPage", name: "one_per_page"
-      label "Break page after each rep (PDF)", for: "one_per_page"
-      br
-      input type: "checkbox", class: "js-grayscale", name: "grayscale"
-      label "Grayscale (PDF)", for: "grayscale"
-    end
   end
 end
