b59585804e3f605e18a3e906a846c319fdfba1ca
John Cipriano <johnmikecip@gmail.com>
2020-02-28 23:04:53 -0500

    remove fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
