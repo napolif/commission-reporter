2244ca59169e6bb1530dcb2e9cfc0d0abbefa9bf
John Cipriano <johnmikecip@gmail.com>
2020-03-01 13:24:15 -0500

    fix active_admin invoice params whitelisting

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Invoice do
   menu priority: 2
 
-  # permit_params :batch, :number, :sales_rep_code, :invoiced_on, :paid_on, :amount, :cost
-  # permit_params :customer_code, :customer_name, :cases, :delivered
+  permit_params :batch, :number, :sales_rep_code, :invoiced_on, :paid_on, :amount, :cost,
+                :customer_code, :customer_name, :cases, :delivered
 end
