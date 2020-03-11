dca1820bed3dd0720033a395d510f512d3162520
John Cipriano <johnmikecip@gmail.com>
2020-03-11 01:32:49 -0400

    add import invoice headers csv

---
+++ app/services/import_invoice_headers_csv.rb
@@ -0,0 +1,20 @@
+# A service for uploading a CSV file with invoice headers (from retalix).
+class ImportInvoiceHeadersCSV < ImportCSV
+  target_class InvoiceHeader
+
+  field_map number:         "HHHINVN",
+            rep_code:       "HHHSLNB",
+            customer_code:  "HHHCUSN",
+            amount:         "HHHINAM",
+            cost:           "HHHEXCR",
+            order_date:     "HHHDTET",
+            qty_ord:        "HHHQYOA"
+
+  index_field number: "HHHINVN"
+
+  def transform_field_order_date(val)
+    Date.strptime(val, "%Y%m%d")
+  rescue
+    nil
+  end
+end
