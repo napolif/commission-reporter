1c43b59612aa06eb6fefabadda3c774abe5e7297
John Cipriano <johnmikecip@gmail.com>
2020-03-16 16:26:08 -0400

    map customers when importing alpha csv

--- app/services/import_alpha_invoice_headers_csv.rb
+++ app/services/import_alpha_invoice_headers_csv.rb
@@ -1,5 +1,7 @@
 # A service for uploading a CSV file with invoice headers (from alpha).
 class ImportAlphaInvoiceHeadersCSV < ImportCSV
+  extend Memoist
+
   target_class InvoiceHeader
 
   field_map number:         "inv_number",
@@ -78,4 +80,18 @@ class ImportAlphaInvoiceHeadersCSV < ImportCSV
   def transform_field_rep_code(val)
     REP_CODE_MAP[val] || val
   end
+
+  def transform_field_customer_code(val)
+    customer_code_map[val] || val
+  end
+
+  def customer_code_map
+    cc_csv = CSV.readlines("../data/customer_mapping.csv", "rb", headers: true)
+    cc_csv.each_with_object({}) do |row, hsh|
+      key = row["ID Alpha"].sub(/^'/, "")
+      val = row["ID Retalix"].sub(/^'/, "")
+      hsh[key] = val
+    end
+  end
+  memoize :customer_code_map
 end
