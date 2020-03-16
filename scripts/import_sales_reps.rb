bceb884f6895af896224606e19e344af17bd87cc
John Cipriano <johnmikecip@gmail.com>
2020-03-16 19:56:22 -0400

    don't disable sales rep in the import script anymore

--- scripts/import_sales_reps.rb
+++ scripts/import_sales_reps.rb
@@ -22,22 +22,3 @@ end
 print_counts(SalesRep) do
   import("../data/sales_reps.csv")
 end
-
-puts "disabling a few..."
-
-# exp
-%w[505 506 513 514 522 523 524 525 526].each do |code|
-  disable(code)
-end
-
-# order takers / inside / etc
-%w[521 533 537 539 540 543 547 548 549 550 551].each do |code|
-  disable(code)
-end
-
-# no longer
-%w[417 530 532].each do |code|
-  disable(code)
-end
-
-puts "done"
