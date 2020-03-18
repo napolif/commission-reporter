3da5416806ff27643dc5594ca0dabcc13e385deb
John Cipriano <johnmikecip@gmail.com>
2020-03-18 00:42:40 -0400

    add specs for import_customers_csv

---
+++ spec/services/import_customers_csv_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe ImportCustomersCSV do
+  let(:csv_path) { path_to_csv("customer.csv") }
+  let(:service) { ImportCustomersCSV.new(csv_path) }
+
+  context "before import" do
+    it "is valid for valid headers" do
+      expect(service).to be_valid
+    end
+  end
+
+  context "with empty Customer table" do
+    it "creates records" do
+      expect { service.run }.to change { Customer.count }.from(0).to(8)
+    end
+
+    it "is still valid after import" do
+      expect { service.run }.not_to change { service.valid? }
+    end
+
+    it "has no errors after import" do
+      service.run
+      expect(service.errors).to be_empty
+    end
+  end
+
+  context "with existing Customer records" do
+    let(:csv_path) { path_to_csv("customer2.csv") }
+    let(:customer) { Customer.find_by(code: "ILLIAN") }
+
+    before { ImportCustomersCSV.new(path_to_csv("customer.csv")).run }
+
+    it "does not create new records" do
+      expect { service.run }.not_to change { Customer.count }
+    end
+
+    it "is still valid after import" do
+      expect { service.run }.not_to change { service.valid? }
+    end
+
+    it "has no errors after import" do
+      service.run
+      expect(service.errors).to be_empty
+    end
+
+    it "updates records where necessary" do
+      expect { service.run }
+        .to(change { customer.reload.name }
+        .from("ILLIANO'S ( WATERFORD )")
+        .to("ILLIANO'S ( WATERBURY )"))
+    end
+  end
+end
