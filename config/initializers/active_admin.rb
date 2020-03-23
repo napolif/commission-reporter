da862482a06ff1b3e455ca265fa7faa02f6c7589
John Cipriano <johnmikecip@gmail.com>
2020-03-23 15:38:31 -0400

    show DEV in title if development

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,11 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Commission Reporter Admin"
+  config.site_title = if Rails.env.development?
+                        "[DEV] Commission Reporter Admin"
+                      else
+                        "Commission Reporter Admin"
+                      end
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
