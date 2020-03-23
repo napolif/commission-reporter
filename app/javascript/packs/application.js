3fa8e9280d73088bae482ec98f88e136445c8179
John Cipriano <johnmikecip@gmail.com>
2020-03-22 23:17:41 -0400

    comment out unused javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,8 @@
 
 require("@rails/ujs").start()
 // require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
 
 import "jquery"
 import "popper.js"
