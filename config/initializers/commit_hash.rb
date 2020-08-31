92fe02179df150df063b05641eea17b78505a504
John Cipriano <johnmikecip@gmail.com>
2020-08-30 19:42:35 -0700

    show commit message in footer

--- config/initializers/commit_hash.rb
+++ config/initializers/commit_hash.rb
@@ -1,2 +1,3 @@
 Rails.configuration.x.commit_hash = `git rev-parse --short HEAD`.chomp
 Rails.configuration.x.commit_date = `git log -1 --format=%ci`.chomp
+Rails.configuration.x.commit_message = `git log -1 --format=%B`.chomp
