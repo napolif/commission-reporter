a0b0b4f190f4cc85059b1894b6273640c3107a07
John Cipriano <johnmikecip@gmail.com>
2021-01-03 20:38:20 -0800

    add more date parsing benchmarks

--- scripts/benchmark_date_parsing.rb
+++ scripts/benchmark_date_parsing.rb
@@ -1,23 +1,35 @@
-n = 1000
+n = 5000
 
-Benchmark.bm do |x|
-  x.report do
+Benchmark.bm(35) do |x|
+  x.report("Date.strptime") do
   	n.times { Date.strptime("20200318", "%Y%m%d") }
   end
 
-  x.report do
+  x.report("Date.strptime + rescue") do
+  	n.times { Date.strptime("20200318", "%Y%m%d") rescue ArgumentError nil }
+  end
+
+  x.report("Date.strptime + rescue *") do
+  	n.times { Date.strptime("20200318", "%Y%m%d") rescue nil }
+  end
+
+  x.report("Time.strptime") do
   	n.times { Time.strptime("20200318", "%Y%m%d") }
   end
 
-  x.report do
+  x.report("Time.strptime + rescue") do
+  	n.times { Time.strptime("20200318", "%Y%m%d") rescue ArgumentError nil }
+  end
+
+  x.report("Timeliness.parse strict + format") do
     n.times { Timeliness.parse("20200318", :date, format: "%Y%m%d", strict: true) }
   end
 
-  x.report do
+  x.report("Timeliness.parse + format") do
     n.times { Timeliness.parse("20200318", format: "%Y%m%d") }
   end
 
-  x.report do
+  x.report("Timeliness.parse") do
     n.times { Timeliness.parse("20200318") }
   end
 end
