c8e44c535e4f82c69940a4d67b91df901b37e770
John Cipriano <johnmikecip@gmail.com>
2022-04-06 18:24:52 -0700

    roll back "tail" in import scripts
    using dates in query manager now

--- scripts/import_retalix_invoice_headers.rb
+++ scripts/import_retalix_invoice_headers.rb
@@ -6,24 +6,10 @@ def print_counts(model_class)
   puts "#{model_class} end count: #{model_class.count}\n"
 end
 
-def tail(file_name)
-  temp = Tempfile.new(['purged', '.csv'])
-  lines = File.readlines(file_name)
-  size = lines.size
-  temp << lines[0]
-  lines[-50000..-1].each { |ln| temp << ln } # TODO: should be date based
-  temp.rewind
-  puts "copied last 50000 lines of #{file_name} to temp file @ #{temp.path}"
-  yield temp
-  temp.close
-  temp.unlink
-end
-
 def import(file_name)
-  tail(file_name) do |file|
-    svc = ImportInvoiceHeadersCSV.new(file)
-    svc.run! rescue binding.pry
-  end
+  file = File.open(file_name, "rb")
+  svc = ImportInvoiceHeadersCSV.new(file)
+  svc.run! # rescue binding.pry
 end
 
 print_counts(InvoiceHeader) do
