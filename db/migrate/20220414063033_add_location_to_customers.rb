150d8d2f8177fdcfda2ef7b3a0a463cc58f3e70a
John Cipriano <johnmikecip@gmail.com>
2022-06-27 08:46:50 +0000

    add default to customer location column

--- db/migrate/20220414063033_add_location_to_customers.rb
+++ db/migrate/20220414063033_add_location_to_customers.rb
@@ -1,6 +1,6 @@
 class AddLocationToCustomers < ActiveRecord::Migration[6.0]
   def change
-    add_column :customers, :location, :integer, null: false
+    add_column :customers, :location, :integer, null: false, default: 6
 
     remove_index :customers, name: "index_customers_on_code"
     add_index :customers, [:code, :location], unique: true
