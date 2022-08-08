053e27002dd39c8f0455aec892c79ba7204e1945
John Cipriano <johnmikecip@gmail.com>
2022-08-07 23:42:23 -0700

    skip location 2 customers again (#5)

--- app/services/import_customers_csv.rb
+++ app/services/import_customers_csv.rb
@@ -11,16 +11,16 @@ class ImportCustomersCSV < ImportCSV
   upsert true
 
   def skip_row?(row)
+    # skip unless company 1 (not sure what company 2 is for but it has duplicates)
+    return true unless row.get("FFDCMPN") == "1"
+
+    # skip unless CT customer
+    return true unless row.get("FFDDIVN") == "6"
+
     # skip if name is blank
     return true if row["FFDCNMB"].blank?
 
-    # skip bad records, e.g. "ILLIAN " which are likely to be duplicates
+    # skip records ending in space, which tend to be duplicates & junk records
     return true if row["FFDCUSN"].end_with? " "
-
-    # skip unless company 1 (not sure what company 2 is for but it has duplicates)
-    return true if row.get("FFDCMPN") != "1"
-
-    # skip unless division 6 / CT
-    row.get("FFDDIVN") != "6"
   end
 end
