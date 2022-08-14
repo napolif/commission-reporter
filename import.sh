e967f1fc4a4ac98b09144a9578f338fde8c70545
John Cipriano <johnmikecip@gmail.com>
2022-08-14 10:13:06 -0700

    clean up import script; use bin/rails

--- import.sh
+++ import.sh
@@ -1,4 +1,8 @@
-echo `time rails runner scripts/import_customers.rb`
-echo `time rails runner scripts/import_purged_records.rb`
-echo `time rails runner scripts/import_retalix_invoice_headers.rb`
-# sudo reboot now
+set -e
+
+for name in import_customers import_purged_records import_retalix_invoice_headers
+do
+  echo $(time bin/rails runner scripts/$name.rb)
+done
+
+sudo reboot now
