cb4203e03eabf3d8148249da2ef0c068d0f1344e
John Cipriano <johnmikecip@gmail.com>
2022-08-15 05:05:12 +0000

    use bundle exec in import shell script

--- import.sh
+++ import.sh
@@ -2,7 +2,7 @@ set -e
 
 for name in import_customers import_purged_records import_retalix_invoice_headers
 do
-  echo $(time bin/rails runner scripts/$name.rb)
+  echo $(time bundle exec rails runner scripts/$name.rb)
 done
 
 sudo reboot now
