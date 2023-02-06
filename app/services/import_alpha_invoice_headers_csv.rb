fe5a44bcb3e46fe1c2f90eb7daa4611b6f393ef2
John Cipriano <johnmikecip@gmail.com>
2023-02-06 00:02:41 -0800

    comment out old & incompatible subclasses

--- app/services/import_alpha_invoice_headers_csv.rb
+++ app/services/import_alpha_invoice_headers_csv.rb
@@ -1,51 +1,51 @@
-# A service for uploading a CSV file with invoice headers (from alpha).
-class ImportAlphaInvoiceHeadersCSV < ImportCSV
-  extend Memoist
-
-  target_class InvoiceHeader
-
-  field_map number:        "inv_number",
-            rep_code:      "sales_rep",
-            customer_code: "cust_code",
-            amount:        "amount",
-            cost:          "cost",
-            order_date:    "maybe_invoice_date",
-            qty_ord:       "total_cases"
-
-  natural_keys [:number]
-
-  upsert true
-
-  csv_options col_sep: "\t",
-              liberal_parsing: true
-
-  def transform_field_order_date(val)
-    Date.strptime(val, "%m/%d/%y") rescue nil
-  end
-
-  def transform_field_rep_code(val)
-    sales_rep_code_map[val] || val
-  end
-
-  def transform_field_customer_code(val)
-    customer_code_map[val] || val
-  end
-
-  def customer_code_map
-    cc_csv = CSV.readlines("../data/customer_mapping.csv", "rb", headers: true)
-    cc_csv.each_with_object({}) do |row, hsh|
-      key = row.get("ID Alpha").sub(/^'/, "")
-      val = row.get("ID Retalix").sub(/^'/, "")
-      hsh[key] = val
-    end
-  end
-  memoize :customer_code_map
-
-  def sales_rep_code_map
-    SalesRep.all.pluck(:alpha_code, :code).each_with_object({}) do |(alf, ret), hsh|
-      next unless alf
-      hsh[alf] = ret
-    end
-  end
-  memoize :sales_rep_code_map
-end
+# # A service for uploading a CSV file with invoice headers (from alpha).
+# class ImportAlphaInvoiceHeadersCSV < ImportCSV
+#   extend Memoist
+#
+#   target_class InvoiceHeader
+#
+#   field_map number:        "inv_number",
+#             rep_code:      "sales_rep",
+#             customer_code: "cust_code",
+#             amount:        "amount",
+#             cost:          "cost",
+#             order_date:    "maybe_invoice_date",
+#             qty_ord:       "total_cases"
+#
+#   natural_keys [:number]
+#
+#   upsert true
+#
+#   csv_options col_sep: "\t",
+#               liberal_parsing: true
+#
+#   def transform_field_order_date(val)
+#     Date.strptime(val, "%m/%d/%y") rescue nil
+#   end
+#
+#   def transform_field_rep_code(val)
+#     sales_rep_code_map[val] || val
+#   end
+#
+#   def transform_field_customer_code(val)
+#     customer_code_map[val] || val
+#   end
+#
+#   def customer_code_map
+#     cc_csv = CSV.readlines("../data/customer_mapping.csv", "rb", headers: true)
+#     cc_csv.each_with_object({}) do |row, hsh|
+#       key = row.get("ID Alpha").sub(/^'/, "")
+#       val = row.get("ID Retalix").sub(/^'/, "")
+#       hsh[key] = val
+#     end
+#   end
+#   memoize :customer_code_map
+#
+#   def sales_rep_code_map
+#     SalesRep.all.pluck(:alpha_code, :code).each_with_object({}) do |(alf, ret), hsh|
+#       next unless alf
+#       hsh[alf] = ret
+#     end
+#   end
+#   memoize :sales_rep_code_map
+# end
--- app/services/import_invoice_summaries_csv.rb
+++ app/services/import_invoice_summaries_csv.rb
@@ -1,42 +1,42 @@
-# A service for uploading the COMMFILE CSV. Similar to active_interaction classes.
-class ImportInvoiceSummariesCSV < ImportCSV
-  extend Memoist
-
-  target_class InvoiceSummary
-
-  field_map number:         "HHUINVN",
-            sales_rep_code: "HHUSLNB",
-            amount:         "HHUEXSN",
-            cost:           "HHUEXCR",
-            customer_code:  "HHUCUSN",
-            customer_name:  "HHUCNMB",
-            cases:          "HHUQYSA",
-            invoiced_on:    "LDATE",
-            paid_on:        "RDATE",
-            delivered:      "HHUINVR"
-
-  natural_keys [:number]
-
-  upsert true
-
-  def generate_records
-    super
-
-    batch_num = InvoiceSummary.next_batch_number + "-csv"
-    records.each do |rec|
-      rec.batch = batch_num
-    end
-  end
-
-  def transform_field_invoiced_on(val)
-    Date.strptime(val, "%m/%d/%Y") rescue nil
-  end
-
-  memoize :transform_field_invoiced_on
-
-  def transform_field_paid_on(val)
-    Date.strptime(val, "%m/%d/%Y") rescue nil
-  end
-
-  memoize :transform_field_paid_on
-end
+# # A service for uploading the COMMFILE CSV. Similar to active_interaction classes.
+# class ImportInvoiceSummariesCSV < ImportCSV
+#   extend Memoist
+#
+#   target_class InvoiceSummary
+#
+#   field_map number:         "HHUINVN",
+#             sales_rep_code: "HHUSLNB",
+#             amount:         "HHUEXSN",
+#             cost:           "HHUEXCR",
+#             customer_code:  "HHUCUSN",
+#             customer_name:  "HHUCNMB",
+#             cases:          "HHUQYSA",
+#             invoiced_on:    "LDATE",
+#             paid_on:        "RDATE",
+#             delivered:      "HHUINVR"
+#
+#   natural_keys [:number]
+#
+#   upsert true
+#
+#   def generate_records
+#     super
+#
+#     batch_num = InvoiceSummary.next_batch_number + "-csv"
+#     records.each do |rec|
+#       rec.batch = batch_num
+#     end
+#   end
+#
+#   def transform_field_invoiced_on(val)
+#     Date.strptime(val, "%m/%d/%Y") rescue nil
+#   end
+#
+#   memoize :transform_field_invoiced_on
+#
+#   def transform_field_paid_on(val)
+#     Date.strptime(val, "%m/%d/%Y") rescue nil
+#   end
+#
+#   memoize :transform_field_paid_on
+# end
