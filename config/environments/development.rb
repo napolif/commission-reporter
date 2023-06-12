22f22b26c5bb6ca9a6099bb9a86d6ae930169a1a
John Cipriano <johnmikecip@gmail.com>
2023-06-12 15:31:42 -0700

    use stdout env var in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
