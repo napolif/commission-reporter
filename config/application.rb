f8cfd60deacef273ddae12c3eac25e5d44c70814
John Cipriano <johnmikecip@gmail.com>
2023-06-12 17:20:36 -0700

    change app time zone to eastern

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module CommissionApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = "Eastern Time (US & Canada)"
+    config.active_record.default_timezone = :utc
+    config.i18n.default_locale = :en
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
