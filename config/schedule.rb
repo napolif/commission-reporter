08073dcca97c03b09004d056d4d407d265200b0a
John Cipriano <johnmikecip@gmail.com>
2023-06-25 21:39:29 -0700

    try get whenever to use rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,8 @@
 set :output, "/home/deploy/commission-app/log/cron.log"
 set :environment, "production"
 
+job_type :rake, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output'
+
 # NOTE: local server time (which cron uses) is eastern
 
 every 1.day, at: "12:30 am" do
